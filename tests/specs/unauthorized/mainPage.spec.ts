e6dd943056b6ff6211fca3843d01a8fa8021aceb
Ruslan8902 <araslanov.r.v@yandex.ru>
2025-08-15 18:02:51 +0500

    add main page fixture

--- tests/specs/unauthorized/mainPage.spec.ts
+++ tests/specs/unauthorized/mainPage.spec.ts
@@ -1,26 +1,14 @@
-import { test } from '@playwright/test';
+import { test, expect } from '../../fixtures/fixtures';
 import { MainPage } from '../../pages/MainPage';
 
-test('Проверка доступности элементов хэдера', async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-  await mainPage.closPopupOverlay();
-  await mainPage.closeCookiesAlert();
+test('Проверка доступности элементов хэдера', async ({ mainPage }) => {
   await mainPage.headerHasCorrectAriaSnapShot();
 });
 
-test('Проверка доступности табов категорий', async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-  await mainPage.closPopupOverlay();
-  await mainPage.closeCookiesAlert();
+test('Проверка доступности табов категорий', async ({ mainPage }) => {
   await mainPage.categoriesTabsHasCorrectAriaSnapShot();
 });
 
-test('Проверка доступности бокового меню', async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-  await mainPage.closPopupOverlay();
-  await mainPage.closeCookiesAlert();
+test('Проверка доступности бокового меню', async ({ mainPage }) => {
   await mainPage.menuHasCorrectAriaSnapShot();
 });
